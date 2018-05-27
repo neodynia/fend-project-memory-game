e06a1703d27534b6058f0223b63f662eb48a1e9c
Ryan Boris <neodynia@icloud.com>
2018-05-27 00:41:53 -0400

    docs: updating commentary

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
 /*jshint esversion: 6 */
 
-/*Variables*/
+/*Global Recurring Variables*/
 let fullDeck = ["diamond", "bolt", "cube", "paper-plane-o", "anchor", "leaf", "bicycle", "bomb", "diamond", "bolt", "cube", "paper-plane-o", "anchor", "leaf", "bicycle", "bomb"];
 let allOpenCards = [];
 let match;
 let moves;
-let seconds = 0; 
-let tens = 0; 
+let seconds = 0;
+let tens = 0;
 let appendTens = document.getElementById("tens");
 let appendSeconds = document.getElementById("seconds");
 let Interval;
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+/*Shuffle function from http://stackoverflow.com/a/2450976*/
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -22,55 +23,54 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
 /*Function to Begin Game Play*/
 function startGame() {
+    // Shuffle Deck 
     let allCards = shuffle(fullDeck);
+    //Empty Board Space for Incoming New Shuffled Cards
     $('.deck').empty();
-    
-
-// Game starts with no matching cards and zero moves
+    // Game starts with no matching cards and zero moves
     match = 0;
     moves = 0;
     $('.moves').text('0');
-
-//Loop to create and place the 16 cards
+    // Loop to create and place the 16 cards
     for (let i = 0; i < allCards.length; i++) {
-       $('.deck').append("<li class=\"card\"><i class=\"fa fa-" + allCards[i] + "\"></i>");
-          }
-       cardMatcher();
+        $('.deck').append("<li class=\"card\"><i class=\"fa fa-" + allCards[i] + "\"></i>");
+    }
+    // Call function that will handle the bulk of the game play
+    cardMatcher();
 }
 
-function startTimer () {
-    tens++; 
-    
-    if(tens < 9){
-      appendTens.innerHTML = "0" + tens;
+/*Game Timer*/
+function startTimer() {
+    // Increment tenth of seconds 
+    tens++;
+    // Display tenths of seconds with only one digit
+    if (tens < 9) {
+        appendTens.innerHTML = "0" + tens;
     }
-    
-    if (tens > 9){
-      appendTens.innerHTML = tens;
-      
-    } 
-    
+    // Display tenths of seconds with two digits
+    if (tens > 9) {
+        appendTens.innerHTML = tens;
+    }
+    // Group 100 (one-tenth seconds) into one second for display and increment seconds
     if (tens > 99) {
-      console.log("seconds");
-      seconds++;
-      appendSeconds.innerHTML = "0" + seconds;
-      tens = 0;
-      appendTens.innerHTML = "0" + 0;
+        console.log("seconds");
+        seconds++;
+        appendSeconds.innerHTML = "0" + seconds;
+        tens = 0;
+        appendTens.innerHTML = "0" + 0;
     }
-    
-    if (seconds > 9){
-      appendSeconds.innerHTML = seconds;
+    // Display 2 digit second values
+    if (seconds > 9) {
+        appendSeconds.innerHTML = seconds;
     }
-  
-  }
-  
+}
 
+/*Star to Moves Made Relationship*/
 function playerRating(moves) {
     if (moves > 9 && moves < 19) {
         $('#star-three').removeClass('fa fa-star');
@@ -85,21 +85,21 @@ function playerRating(moves) {
 
 
 //End of game stats on modal
-function gameOver(moves,score) {
+function gameOver(moves, score) {
     $('#winnerText').text(`Moves : ${moves} - Time: ${seconds}.${tens} seconds`);
     if (moves > 9 && moves < 19) {
         $('span.starCount').text('THREE STARS!');
     } else if (moves > 19 && moves < 29) {
         $('span.starCount').text('TWO STARS!');
     } else if (moves > 29) {
-        $('span.starCount').text('ONE STAR!'); 
+        $('span.starCount').text('ONE STAR!');
     }
 
     $("#myModal").modal();
 
 
-//restart game
-    $('.restart').bind('click', function(confirmed) {
+    //restart game
+    $('.restart').bind('click', function (confirmed) {
         if (confirmed) {
             $('.stars').removeClass('fa fa-star-o').addClass('fa fa-star');
             clearInterval(Interval);
@@ -112,56 +112,59 @@ function gameOver(moves,score) {
     });
 }
 //match like cards and indicate matches and nonmatches
-var cardMatcher = function() {
+var cardMatcher = function () {
     //check card to see if flipped
-    $('.card').on('click', function() {
+    $('.card').on('click', function () {
         clearInterval(Interval);
         Interval = setInterval(startTimer, 10);
         if ($(this).hasClass('show') || $(this).hasClass('match')) {
-            return true;}
+            return true;
+        }
         //push HTML of each card into an array
         let box = $(this).html();
         $(this).addClass('open show').removeClass('animated shake bounce');
         allOpenCards.push(box);
 
         //turn off clicks when two cards are open 
-    if (allOpenCards.length >= 2) {$('.card').off('click');} 
-       //match cards 
+        if (allOpenCards.length >= 2) {
+            $('.card').off('click');
+        }
+        //match cards 
         if (allOpenCards.length > 1) {
-        //match cards with animated bounce
+            //match cards with animated bounce
             if (allOpenCards[1] === allOpenCards[0]) {
-                    $('.deck').find('.open').addClass('match animated bounce').removeClass('shake open');
-                    match++;
-                    allOpenCards.length = 0;
-                    cardMatcher();
-       
-        //Delay cards if not matched then cover up with animated shake
-            } else  {
+                $('.deck').find('.open').addClass('match animated bounce').removeClass('shake open');
+                match++;
+                allOpenCards.length = 0;
+                cardMatcher();
+
+                //Delay cards if not matched then cover up with animated shake
+            } else {
                 $('.deck').find('.open').addClass('animated shake');
-                setTimeout(function() {
+                setTimeout(function () {
                     $('.deck li.card').removeClass('open show animated shake bounce');
                     allOpenCards.length = 0;
                     cardMatcher();
                 }, 1200);
-         
+
             }
-        
-        moves++;
-        playerRating(moves);
-        $('.moves').html(moves);
+
+            moves++;
+            playerRating(moves);
+            $('.moves').html(moves);
         }
 
         if (match === 8) {
             clearInterval(Interval);
             playerRating(moves);
             let score = playerRating(moves).score;
-            setTimeout(function() {
-                gameOver(moves,score);
+            setTimeout(function () {
+                gameOver(moves, score);
             }, 500);
         }
     });
 };
-$(document).ready(function() {  
-startGame();
+$(document).ready(function () {
+    startGame();
 
 });
